76917fd63b3ff02a2f57f3754435e211d8d73612
Wilson Callisaya <demarktec@gmail.com>
2022-11-05 19:11:39 -0500

    Proyecto blanco node inicio

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const res = require('express/lib/response');
+const app = express();
+
+app.get('/',(req,res)=>res.send("Hola mundo")
+);
+
+app.listen(3004);
+console.log("Server on port ", 3004)
