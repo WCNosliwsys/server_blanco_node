f09acd7587ab731123893f79065ef6a4c080e501
Wilson Callisaya <demarktec@gmail.com>
2022-11-05 19:55:49 -0500

    modificado por segunda

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 
-app.get('/',(req,res)=>res.send("Hola mundo modificado por wilson")
+app.get('/',(req,res)=>res.send("Hola mundo modificado por wilson por segunda vez")
 );
 
 app.listen(3004);
