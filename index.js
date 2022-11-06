fc2e018d7ca1fc05ae28f20bb9fe28e7312da78e
Wilson Callisaya <demarktec@gmail.com>
2022-11-05 19:41:41 -0500

    modificado por

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 
-app.get('/',(req,res)=>res.send("Hola mundo")
+app.get('/',(req,res)=>res.send("Hola mundo modificado por wilson")
 );
 
 app.listen(3004);
